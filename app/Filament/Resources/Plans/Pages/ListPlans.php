0d5ba23b5de853dc93da7b1480b051ae9926bc40
Ahmed Kalalsh <Ahmedkalash513@gmail.com>
2026-04-18 00:42:05 +0200

    feat: add PlanResource and update UserResource

--- app/Filament/Resources/Plans/Pages/ListPlans.php
+++ app/Filament/Resources/Plans/Pages/ListPlans.php
@@ -13,7 +13,7 @@ class ListPlans extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+//            CreateAction::make(),
         ];
     }
 }
